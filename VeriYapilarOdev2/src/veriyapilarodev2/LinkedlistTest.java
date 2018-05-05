edd18cf42e312daeaec8c2c5a09beca032833991
AbderrhmanAbdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2018-05-05 23:39:49 +0300

    jar

--- VeriYapilarOdev2/src/veriyapilarodev2/LinkedlistTest.java
+++ VeriYapilarOdev2/src/veriyapilarodev2/LinkedlistTest.java
@@ -267,6 +267,7 @@ public class LinkedlistTest extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new LinkedlistTest().setVisible(true);
             }
